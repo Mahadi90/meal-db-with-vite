c33079e11e5bd1b39c8ff82a9b33a612bdaa9c13
Mahadi90 <mahadimhs787@gmail.com>
2023-04-04 10:25:06 +0600

    about added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='bg-gray-700 h-[80vh]'>
+        <div className='bg-gray-700 h-[100vh] md:h-[80vh]'>
            <h1 className='text-5xl font-bold text-white text-center pt-20 '>Awesome Item from MealDb you can get,If you want!</h1>
 
            <p className=' text-white py-12 text-center px-2 md:px-12 lg:px-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi labore quos nihil repellat. Doloribus natus cumque dicta libero deserunt nesciunt vero nostrum perferendis laudantium, iusto excepturi modi esse pariatur assumenda minima vitae tenetur odit perspiciatis deleniti. Eius, quam! Vitae itaque labore aspernatur corporis, neque mollitia provident quos fuga. Harum, odio!</p>
